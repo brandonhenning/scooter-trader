f64d583d14edf9554980168cfbf52a862dc1f1da
Brandon Henning <brandonhenning@Brandons-MacBook-Pro-3.local>
2018-05-31 13:47:00 -0600

    removed comments from models

--- models/user.js
+++ models/user.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-// Define our scooter schema
+// Define our user schema
 const UserSchema = new mongoose.Schema({
     username: String,
     password: String
